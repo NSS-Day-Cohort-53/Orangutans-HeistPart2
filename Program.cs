e124c11bc91bae20a05c4f878adf45809990a621
Sara Hackle <Shackle@ivytech.edu>
2022-03-15 13:09:27 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Orangutans_HeistPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
